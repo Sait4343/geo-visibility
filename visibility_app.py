ba45ff6b87ecd2d6a3555f19a91863064bcec409
Sait4343 <70326025+Sait4343@users.noreply.github.com>
2025-12-16 22:16:31 +0200

    вебхук для рек продакшн

--- visibility_app.py
+++ visibility_app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 # 🔴 ПРОДАКШН N8N ВЕБХУКИ
 N8N_GEN_URL = "https://virshi.app.n8n.cloud/webhook/webhook/generate-prompts"
 N8N_ANALYZE_URL = "https://virshi.app.n8n.cloud/webhook/webhook/run-analysis_prod"
-N8N_RECO_URL = "https://virshi.app.n8n.cloud/webhook-test/recommendations"  # за потреби заміниш
+N8N_RECO_URL = "https://virshi.app.n8n.cloud/webhook/recommendations"  
 N8N_CHAT_WEBHOOK = "https://virshi.app.n8n.cloud/webhook/webhook/chat-bot" 
 
 
